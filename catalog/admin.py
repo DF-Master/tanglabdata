51ef30f9c4fb7b558f62855c68bd787ec762b6ba
Yida Jiang <69508342+DF-Master@users.noreply.github.com>
2023-11-17 15:11:03 +0800

    Update admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -9,96 +9,97 @@ admin.site.register(Tags)
 
 # ForeignKey
 
-
-
-
 # admin.site.register(Reagent)
 # admin.site.register(ReagentInstance)
 ## Register the Admin classes using the decorator
 
+
 class ReagentInstanceInline(admin.TabularInline):
     model = ReagentInstance
-    extra=0
-    
+    extra = 0
+
+
 class ReagentInline(admin.TabularInline):
     model = Reagent
-    extra=0
+    extra = 0
+
 
 @admin.register(Reagent)
 class ReagentAdmin(admin.ModelAdmin):
-    list_display = (
-        'name',
-        'chinese_name',
-        'english_name',
-        "display_tags"
-    )
-    search_fields=('name','chinese_name__name','english_name__name','purchase_note','note')
+    list_display = ('name', 'chinese_name', 'english_name', "display_tags")
+    search_fields = ('name', 'chinese_name__name', 'english_name__name',
+                     'purchase_note', 'note')
+    autocomplete_fields = ["chinese_name", "english_name", "source"]
     list_filter = ('source', 'tags')
     inlines = [ReagentInstanceInline]
     fieldsets = (
         (None, {
-            'fields': ('name', 'chinese_name','english_name','tags')
+            'fields': ('name', 'chinese_name', 'english_name', 'tags')
         }),
         ('Source', {
-            'fields': ('source', 'cas','specification')
+            'fields': ('source', 'cas', 'specification')
         }),
         ('Others', {
-            'fields': ('purchase_note','note')
+            'fields': ('purchase_note', 'note')
         }),
     )
+
 
 ## Register the Admin classes for Instance using the decorator
 
 
 @admin.register(ReagentInstance)
 class ReagentInstanceAdmin(admin.ModelAdmin):
-    list_display = (
-        'name',
-        'status', 'register_date'
-    )
-    list_filter = ('status', 'register_date','principal')
-    search_fields=('reagent__name','name','id','location__name','principal__first_name','principal__last_name','note')
+    list_display = ('name', 'status', 'register_date')
+    list_filter = ('status', 'register_date', 'principal')
+    search_fields = ('reagent__name', 'name', 'id', 'location__name',
+                     'principal__first_name', 'principal__last_name', 'note')
+    autocomplete_fields = ["reagent", "location", "principal"]
     fieldsets = (
         (None, {
-            'fields': ('reagent','name', 'id','location','principal')
+            'fields': ('reagent', 'name', 'id', 'location', 'principal')
         }),
         ('Availability', {
             'fields': ('status', 'register_date')
         }),
         ('Others', {
-            'fields': ('note',)
+            'fields': ('note', )
         }),
-
     )
-    
+
+
 # Only one name
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
     inlines = [ReagentInstanceInline]
-    search_fields=('name',)
-  
+    search_fields = ('name', )
+
+
 @admin.register(ChineseName)
 class ChineseNameAdmin(admin.ModelAdmin):
     inlines = [ReagentInline]
-    search_fields=('name',)
-    
+    search_fields = ('name', )
+
+
 @admin.register(EnglishName)
 class EnglishNameAdmin(admin.ModelAdmin):
     inlines = [ReagentInline]
-    search_fields=('name',)
-    
+    search_fields = ('name', )
+
+
 @admin.register(Source)
 class SourceAdmin(admin.ModelAdmin):
     inlines = [ReagentInline]
-    search_fields=('name',)
-    
-    
+    search_fields = ('name', )
+
+
 # admin.site.register(Principal)
 ## Define the admin class
 class PrincipalAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name')
     inlines = [ReagentInstanceInline]
     # fields = ['first_name', 'last_name']
+
 
 ## Register the admin class with the associated model
 admin.site.register(Principal, PrincipalAdmin)
